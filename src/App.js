87936cef7986c9b5368b6771bb35cb85202942c5
Naman Agarwal <niffleheim22@gmail.com>
2018-06-24 19:15:50 -0400

    integrated react-responsive

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 } from '@appbaseio/reactivesearch';
 import './App.css';
 import { Card, CardColumns, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col} from 'reactstrap';
+import MediaQuery from 'react-responsive';
 
 class App extends Component {
   dateFormat(date) {
@@ -72,90 +73,199 @@ class App extends Component {
           />
         </div>
         <div className={"display"}>
-          <div className={"leftSidebar"}>
-            <RangeSlider
-              componentId="ratingsFilter"
-              dataField="score"
-              title="Claimbuster Score"
-              range={{
-                "start": 0,
-                "end": 100
-              }}
-              defaultSelected={{
-                "start": 50,
-                "end": 100
-              }}
-              showHistogram={true}
-              stepValue={1}
-              interval={5}
-            />
-            <DateRange
-              componentId="dateFilter"
-              dataField="date"
-              title="Date Tweeted"
-              numberOfMonths={1}
-              queryFormat = "date"
-            />
-            <MultiList
-              style={{ marginTop: 1.5 + "em" }}
-              componentId="speakerFilter"
-              dataField="prefname.raw"
-              title="Speakers"
-              showCheckbox={true}
-              showCount={true}
-              placeholder = "Filter by tweet authors"
-              sortBy="asc"
-            />
-          </div>
-          <div className={"mainBar"}>
-            <ReactiveList
-              componentId="results"
-              className = "results"
-              innerClass = {{
-                "pagination": "resultpages"
-              }}
-              dataField="prefname"
-              react={{
-                "and": ["mainSearch", "ratingsFilter", "speakerFilter", "dateFilter"]
-              }}
-              pagination={true}
-              size={12}
-              onAllData={(results) =>
-                <div style={{ margin: 0.5 + "em" }}>
-                  <CardColumns>
-                    {results.map((res, index) =>
-                      <div style={{ margin: 0.25 + "em" }} key={index}>
-                        <Card>
-                          <CardBody>
-                            <CardTitle>{res.prefname} - @{res.account.toString().toLowerCase()}</CardTitle>
-                            <CardSubtitle>{this.dateFormat(res.date)}</CardSubtitle>
-                            <CardText style={{ marginTop: 0.5 + "em" }}>{res.claim}</CardText>
-                            <Row>
-                              <Col sm="6" md="6">
-                                <Button tag="a" outline color="primary" href={res.link}>Link</Button>
-                              </Col>
-                              <Col sm="6" md="6">
-                                <CardText>Score: {Number(res.score.toFixed(2))}</CardText>
-                              </Col>
-                            </Row>
-                          </CardBody>
-                        </Card>
-                      </div>
-                    )}
-                  </CardColumns>
-                </div>
+          <MediaQuery minWidth={801} >
+            <div className={"leftSidebar"}>
+              <RangeSlider
+                componentId="ratingsFilter"
+                dataField="score"
+                title="Claimbuster Score"
+                range={{
+                  "start": 0,
+                  "end": 100
+                }}
+                defaultSelected={{
+                  "start": 50,
+                  "end": 100
+                }}
+                showHistogram={true}
+                stepValue={1}
+                interval={5}
+              />
+              <DateRange
+                componentId="dateFilter"
+                dataField="date"
+                title="Date Tweeted"
+                numberOfMonths={1}
+                queryFormat="date"
+              />
+              <MultiList
+                style={{ marginTop: 1.5 + "em" }}
+                componentId="speakerFilter"
+                dataField="prefname.raw"
+                title="Speakers"
+                showCheckbox={true}
+                showCount={true}
+                placeholder="Filter by tweet authors"
+                sortBy="asc"
+              />
+            </div>
+          </MediaQuery>
+          <MediaQuery minWidth={801}>
+            <div className={"mainBar"}>
+              <ReactiveList
+                componentId="results"
+                className="results"
+                innerClass={{
+                  "pagination": "resultpages"
+                }}
+                dataField="prefname"
+                react={{
+                  "and": ["mainSearch", "ratingsFilter", "speakerFilter", "dateFilter"]
+                }}
+                pagination={true}
+                size={12}
+                onAllData={(results) =>
+                  <div style={{ margin: 0.5 + "em" }}>
+                    <CardColumns>
+                      {results.map((res, index) =>
+                        <div style={{ margin: 0.25 + "em" }} key={index}>
+                          <Card>
+                            <CardBody>
+                              <CardTitle>{res.prefname} - @{res.account.toString().toLowerCase()}</CardTitle>
+                              <CardSubtitle>{this.dateFormat(res.date)}</CardSubtitle>
+                              <CardText style={{ marginTop: 0.5 + "em" }}>{res.claim}</CardText>
+                              <Row>
+                                <Col sm="6" md="6">
+                                  <Button tag="a" outline color="primary" href={res.link}>Link</Button>
+                                </Col>
+                                <Col sm="6" md="6">
+                                  <CardText>Score: {Number(res.score.toFixed(2))}</CardText>
+                                </Col>
+                              </Row>
+                            </CardBody>
+                          </Card>
+                        </div>
+                      )}
+                    </CardColumns>
+                  </div>
                 }
-              sortOptions={[
-                { dataField: "_score", sortBy: "desc", label: "Best Match" },
-                { dataField: "score", sortBy: "desc", label: "Claimbuster Score (High to low)" },
-                { dataField: "score", sortBy: "asc", label: "Claimbuster Score (Low to High)" },
-                { dataField: "prefname.raw", sortBy: "asc", label: "Speaker A->Z" },
-                { dataField: "prefname.raw", sortBy: "desc", label: "Speaker Z->A" },
-                { dataField: "date", sortBy: "desc", label: "Newest" },
-                { dataField: "date", sortBy: "asc", label: "Oldest" }
-              ]}
-            />
-          </div>
+                sortOptions={[
+                  { dataField: "_score", sortBy: "desc", label: "Best Match" },
+                  { dataField: "score", sortBy: "desc", label: "Claimbuster Score (High to low)" },
+                  { dataField: "score", sortBy: "asc", label: "Claimbuster Score (Low to High)" },
+                  { dataField: "prefname.raw", sortBy: "asc", label: "Speaker A->Z" },
+                  { dataField: "prefname.raw", sortBy: "desc", label: "Speaker Z->A" },
+                  { dataField: "date", sortBy: "desc", label: "Newest" },
+                  { dataField: "date", sortBy: "asc", label: "Oldest" }
+                ]}
+              />
+            </div>
+          </MediaQuery>
+          <MediaQuery maxWidth={800}>
+            <div className={"mainBar"}>
+              <ReactiveList
+                componentId="results"
+                className="results"
+                innerClass={{
+                  "pagination": "resultpages"
+                }}  
+                dataField="prefname"
+                react={{
+                  "and": ["mainSearch", "ratingsFilter", "speakerFilter", "dateFilter"]
+                }}
+                pagination={true}
+                size={5}
+                onAllData={(results) =>
+                  <div style={{ margin: 0.5 + "em" }}>
+                    <CardColumns>
+                      {results.map((res, index) =>
+                        <div style={{ margin: 0.25 + "em" }} key={index}>
+                          <Card>
+                            <CardBody>
+                              <CardTitle>{res.prefname} - @{res.account.toString().toLowerCase()}</CardTitle>
+                              <CardSubtitle>{this.dateFormat(res.date)}</CardSubtitle>
+                              <CardText style={{ marginTop: 0.5 + "em" }}>{res.claim}</CardText>
+                              <Row>
+                                <Col sm="6" md="6">
+                                  <Button tag="a" outline color="primary" href={res.link}>Link</Button>
+                                </Col>
+                                <Col sm="6" md="6">
+                                  <CardText>Score: {Number(res.score.toFixed(2))}</CardText>
+                                </Col>
+                              </Row>
+                            </CardBody>
+                          </Card>
+                        </div>
+                      )}
+                    </CardColumns>
+                  </div>
+                }
+                sortOptions={[
+                  { dataField: "_score", sortBy: "desc", label: "Best Match" },
+                  { dataField: "score", sortBy: "desc", label: "Claimbuster Score (High to low)" },
+                  { dataField: "score", sortBy: "asc", label: "Claimbuster Score (Low to High)" },
+                  { dataField: "prefname.raw", sortBy: "asc", label: "Speaker A->Z" },
+                  { dataField: "prefname.raw", sortBy: "desc", label: "Speaker Z->A" },
+                  { dataField: "date", sortBy: "desc", label: "Newest" },
+                  { dataField: "date", sortBy: "asc", label: "Oldest" }
+                ]}
+              />
+              <p>This app is best viewed on larger devices like a computer or a horizontally held tablet!</p>
+            </div>
+          </MediaQuery>
+          <MediaQuery maxWidth={482}>
+            <div className={"mainBar"}>
+              <ReactiveList
+                componentId="results"
+                className="results"
+                innerClass={{
+                  "pagination": "resultpages"
+                }}
+                dataField="prefname"
+                react={{
+                  "and": ["mainSearch", "ratingsFilter", "speakerFilter", "dateFilter"]
+                }}
+                pagination={true}
+                paginationAt="top"
+                size={2}
+                onAllData={(results) =>
+                  <div style={{ margin: 0.5 + "em" }}>
+                    <CardColumns>
+                      {results.map((res, index) =>
+                        <div style={{ margin: 0.25 + "em" }} key={index}>
+                          <Card className="card">
+                            <CardBody>
+                              <CardTitle>{res.prefname} - @{res.account.toString().toLowerCase()}</CardTitle>
+                              <CardSubtitle>{this.dateFormat(res.date)}</CardSubtitle>
+                              <CardText style={{ marginTop: 0.5 + "em" }}>{res.claim}</CardText>
+                              <Row>
+                                <Col sm="6" md="6">
+                                  <Button tag="a" outline color="primary" href={res.link}>Link</Button>
+                                </Col>
+                                <Col sm="6" md="6">
+                                  <CardText>Score: {Number(res.score.toFixed(2))}</CardText>
+                                </Col>
+                              </Row>
+                            </CardBody>
+                          </Card>
+                        </div>
+                      )}
+                    </CardColumns>
+                  </div>
+                }
+                sortOptions={[
+                  { dataField: "_score", sortBy: "desc", label: "Best Match" },
+                  { dataField: "score", sortBy: "desc", label: "Claimbuster Score (High to low)" },
+                  { dataField: "score", sortBy: "asc", label: "Claimbuster Score (Low to High)" },
+                  { dataField: "prefname.raw", sortBy: "asc", label: "Speaker A->Z" },
+                  { dataField: "prefname.raw", sortBy: "desc", label: "Speaker Z->A" },
+                  { dataField: "date", sortBy: "desc", label: "Newest" },
+                  { dataField: "date", sortBy: "asc", label: "Oldest" }
+                ]}
+              />
+              <p>This app is best viewed on larger devices like a computer or a horizontally held tablet!</p>
+            </div>
+          </MediaQuery>
         </div>
       </ReactiveBase>
     );
